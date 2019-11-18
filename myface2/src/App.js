59c65247c451be24017c939ed23a634223718cda
ChrisBRN <>
2019-11-18 16:44:17 +0000

    JSON Parsing

--- myface2/src/App.js
+++ myface2/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './newlogo-non-transparent.png';
-import './App.css';
+import './App.scss';
 
 import {
   BrowserRouter as Router,
@@ -12,71 +12,123 @@ import {
 function App() {
 
   return (
-    <Router>
-      
-    <NavBar/>      
-    
+    <Router>       
     <Switch>        
-      <Route path="/users"> <Users /> </Route>
-      <Route path="/posts"> <Posts /> </Route>
-      <Route path="/">      <Home />  </Route>
-    </Switch>  
-
-    <Footer/>  
-
+      <Route path="/users"> <UsersPage /> </Route>
+      <Route path="/posts"> <PostsPage /> </Route>
+      <Route path="/home">  <HomePage /> </Route>
+      <Route path="/">      <IndexPage /> </Route>
+    </Switch> 
   </Router>
 
 );
 
+//  ============================================================================
+//  Helpers ====================================================================
+//  ============================================================================
 
+function WelcomeMessage(props){
 
-
-function Welcome(props){
+  let message = "Welcome to MyFace 2";
 
   if (props){
-    return <h1>Welcome Back {props.name}</h1>
-  }
-  else {
-    return <h1>Welcome to MyFace 2</h1>
-  }
+    message = "Welcome Back " + props.name
+  }  
+
+  return <h1><Link to="/home">{message}</Link></h1>
 }
 
 function GetName(){  
   return "Dave" // TODO Flesh out once login/users is working
 }
 
-function Home() {  
+async function FetchJson(){
+
+  let url = 'http://localhost:8080/api/posts';
+  let response = await fetch(url); 
+  let posts = response.json(); 
+      
+  let post = JSON.parse(posts, function(key, value) {
+      if (key === id) {
+        return value; 
+      }
+      
+    }
+  )
+  return post;
+
+  // return post (
+  
+  //   // <div>
+
+  //   //   {this.state.arrayFromJson.map(function(posts) {
+  //   //     return (
+  //   //       <SomeComponent key={posts.id} data={posts} />
+  //   //     );
+
+  //   //   })}  
+  //   // </div>
+  // )
+}
+
+//  ============================================================================
+//  Pages   ====================================================================
+//  ============================================================================
+
+function IndexPage() {  
   return (    
-    <div className="Home-page">
-      <Welcome name={GetName()} />      
+    <div className="Welcome-page">
+      <WelcomeMessage name={GetName()} />          
     </div>
   ) 
 }
 
-function Posts() {
-  return <h2>Posts</h2>;
+function HomePage() {
+  return (
+    <div>
+      <NavBar/> 
+      <h2 className="Page-Title">Home</h2>
+      <Footer/>
+    </div>
+  )    
 }
 
-function Users() {
-  return <h2>Users</h2>;
+function PostsPage() { 
+
+  return (
+    <div>
+      <NavBar/> 
+      
+      <h2>Posts {FetchJson()} </h2>
+      <Footer/>
+    </div>
+  )  
+}
+
+function UsersPage() {
+  return  (
+    <div>
+      <NavBar/> 
+      <h2>Users</h2>
+      <Footer/>
+    </div>
+  )
 }  
 
-}
+//  ============================================================================
+//  Sections    ================================================================
+//  ============================================================================
 
 function NavBar(){
   return (
 
     <div className="Nav-bar" id="Header-nav-bar">
-      <nav>
-        {/* TODO Logo */}
-        
-        <Link to="/"><img className="logo" src={logo} alt="logo"/></Link>        
+      <nav>    
+        <Link to="/home"><img className="logo" src={logo} alt="logo"/></Link>        
         <Link to="/posts">Posts</Link>
         <Link to="/users">Users</Link>
-
       </nav>
     </div>
-
   )
 }
 
@@ -84,12 +136,11 @@ function Footer(){
   return (
 
     <div className="Nav-bar" id="Footer-nav-bar">
-      <nav>
-        {/* TODO Logo */}
-        FOOTER 
-        <Link to="/">Home</Link>        
-        <Link to="/posts">Posts</Link>
-        <Link to="/users">Users</Link>
+      <nav>        
+        FOOTER   
+        <Link to="/"> Home </Link>        
+        <Link to="/posts"> Posts </Link>
+        <Link to="/users"> Users </Link>
 
         {/* Other Footer Text */}
       </nav>
@@ -97,5 +148,5 @@ function Footer(){
   ) 
 }
 
-
+}
 export default App;
