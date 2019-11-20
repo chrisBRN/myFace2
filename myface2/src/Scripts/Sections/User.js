112ca214372933759f0dd8bdbc800690f8f34fba
ChrisBRN <>
2019-11-20 16:24:08 +0000

    Styles

--- myface2/src/Scripts/Sections/User.js
+++ myface2/src/Scripts/Sections/User.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+
+import TempImage from '../Helpers/ImageExists.js'
+
 import '../../Styles/user.scss';
 
-export default function User(){       
-    let {userId} = useParams();
 
+async function fetchUserInfo(param) {
+    const response = await fetch('http://localhost:8080/api/users/' + param);
+    return await response.json();
+}
+
+export default function User(){       
+    
+    let {userId} = useParams();
     const [userInfo, setUserInfo] = useState(null);
 
     useEffect(
@@ -17,15 +26,18 @@ export default function User(){
     }
     
     return (  
-        <div>
-            <h1>Yo</h1> 
-            <div>{userInfo.username}</div>
+        <div>            
+             <TempImage className="bannerImage" src={userInfo.bannerImage} />
+             <TempImage className="profileImage" src={userInfo.profileImage} />
+             <h1 class="username">{userInfo.username.toUpperCase()}</h1>
+             <div class="infoBox">
+                <h2 class="realname">{userInfo.firstName + " " + userInfo.lastName}</h2>
+                <p class="contact">{"Contact: " + userInfo.email}</p>
+             </div>
+             <div class="postslist">
+                 
+             </div>
         </div>
     );
 }
 
-
-async function fetchUserInfo(param) {
-    const response = await fetch('http://localhost:8080/api/users/' + param);
-    return await response.json();
-}
